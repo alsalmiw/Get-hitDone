b85490343635d6e5b71804115e0eefdb99c06d8b
DannyHT04 <dannyhat1@gmail.com>
2022-03-14 11:24:27 -0700

    added cors

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using get_shit_done_webapi.Services;
 using get_shit_done_webapi.Services.Context;
+using Microsoft.EntityFrameworkContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,21 @@ builder.Services.AddScoped <ProjectService>();
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<TaskService>();
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("GetShitDoneString");
+builder.Services.AddDbContext<DataContext>(option => option.UseSqlServer(connectionString));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("GetShitDonePolicy",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+app.UseCors("GetShitDonePolicy");
 
 app.UseAuthorization();
 
