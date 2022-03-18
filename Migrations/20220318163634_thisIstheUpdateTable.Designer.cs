a58de824880fd0c5c49bb2da17ff41fd35876bf0
pVang8 <pvang@codestack.co>
2022-03-18 10:02:04 -0700

    test

--- Migrations/20220318163634_thisIstheUpdateTable.Designer.cs
+++ Migrations/20220318163634_thisIstheUpdateTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using get_shit_done_webapi.Services.Context;
 
@@ -11,9 +12,10 @@ using get_shit_done_webapi.Services.Context;
 namespace get_shit_done_webapi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220318163634_thisIstheUpdateTable")]
+    partial class thisIstheUpdateTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
